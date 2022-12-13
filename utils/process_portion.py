951eeb7a5b7bead319ca824602f932dc0c2e1ae5
surface@pro.3 <348699103@qq.com>
2022-12-13 09:15:38 +0800

    增加间隔

--- utils/process_portion.py
+++ utils/process_portion.py
@@ -32,7 +32,7 @@ def get_diffrent_intervals_at_once(timestamps, max_coverage):
         coverage = coverage_ + 1
         if (coverage > 10) and (coverage % 30 != 0):
             continue
-        if (coverage <= 10) and (coverage not in [1, 10]):
+        if (coverage <= 10) and (coverage not in [1, 5, 10]):
             continue
         ep = ExplicitPortion(coverage)
         for timestamp in timestamps:
